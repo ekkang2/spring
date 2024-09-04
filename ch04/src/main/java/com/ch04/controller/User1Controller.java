7190b2dbc43c82629387490b5a9008066eab9af1
chhak0503 <chhak0503@gmail.com>
2024-09-04 10:25:41 +0900

    spring jdbc study

--- ch04/src/main/java/com/ch04/controller/User1Controller.java
+++ ch04/src/main/java/com/ch04/controller/User1Controller.java
@@ -1,7 +1,9 @@
 package com.ch04.controller;
 
 import com.ch04.dto.User1DTO;
+import com.ch04.service.User1Service;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,17 +11,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class User1Controller {
 
+    private User1Service service;
+
+    @Autowired
+    public User1Controller(User1Service service) {
+        this.service = service;
+    }
+
     @GetMapping("/user1/register")
     public String register(){
         return "/user1/register";
     }
 
     @PostMapping("/user1/register")
-    public void register(User1DTO dto){
+    public String register(User1DTO dto){
         System.out.println(dto);
 
+        // 등록
+        service.insertUser1(dto);
 
-
+        // 리다이렉트
+        return "redirect:/user1/list";
     }
 
 
--- ch04/src/main/java/com/ch04/dao/User1DAO.java
+++ ch04/src/main/java/com/ch04/dao/User1DAO.java
@@ -2,6 +2,8 @@ package com.ch04.dao;
 
 
 import com.ch04.dto.User1DTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +11,28 @@ import java.util.List;
 @Repository
 public class User1DAO {
 
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public User1DAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void insertUser1(User1DTO dto){
 
+        String sql = "insert into `user1` values (?,?,?,?,?)";
+        Object[] params = {
+                dto.getUid(),
+                dto.getName(),
+                dto.getBirth(),
+                dto.getHp(),
+                dto.getAge()
+            };
+
+        jdbcTemplate.update(sql, params);
     }
+
     public User1DTO selectUser1(String uid){
         return null;
     }
