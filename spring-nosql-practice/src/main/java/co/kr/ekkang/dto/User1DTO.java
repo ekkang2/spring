7c631de8e6af808a4ec93653b4cdecc15459a0e9
ekkang2 <rkddmsrud27@gmail.com>
2024-10-08 16:35:13 +0900

    add new

---
+++ spring-nosql-practice/src/main/java/co/kr/ekkang/dto/User1DTO.java
@@ -0,0 +1,21 @@
+package co.kr.ekkang.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User1DTO {
+
+    private String uid;
+    private String name;
+    private int age;
+    private String addr;
+
+
+
+}
---
+++ spring-redis-practice/src/main/java/co/kr/ekkang/controller/MainController.java
@@ -0,0 +1,86 @@
+package co.kr.ekkang.controller;
+
+import co.kr.ekkang.service.MainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@RestController
+public class MainController {
+
+    private final MainService mainService;
+
+    @PostMapping("/redis/string")
+    public void setValue(String key, String value) {
+        mainService.setValue(key, value);
+    }
+
+    @GetMapping("/redis/string/{key}")
+    public ResponseEntity<String> getValue(@PathVariable("key") String key){
+        String value = mainService.getValue(key);
+        return ResponseEntity
+                .ok()
+                .body(value);
+    }
+
+    // 큐 용(first in first out 터널)
+    @PostMapping("/redis/list-right")
+    public void addToListFromRight(String key, String value){
+        mainService.addToListFromRight(key, value);
+    }
+
+    // 스택 용(last in first out 블럭쌓기)
+    @PostMapping("/redis/list-left")
+    public void addToListFromLeft(String key, String value){
+        mainService.addToListFromLeft(key, value);
+    }
+
+    @GetMapping("/redis/list/{key}/{index}")
+    public ResponseEntity<String> getFromList(@PathVariable("key") String key, @PathVariable("index") int index){
+        String value = mainService.getFromList(key, index);
+        return ResponseEntity
+                .ok()
+                .body(value);
+    }
+
+    @GetMapping("/redis/list/{key}/{start}/{end}")
+    public ResponseEntity<List<String>> getRangeFromList(@PathVariable("key") String key,
+                                                         @PathVariable("start") int start,
+                                                         @PathVariable("end") int end){
+        List<String> values = mainService.getRangeFromList(key, start, end);
+        return ResponseEntity
+                .ok()
+                .body(values);
+    }
+
+    @PostMapping("/redis/set")
+    public void addToSet(String key, String[] values){
+        mainService.addToSet(key, values);
+    }
+
+    @GetMapping("/redis/set/{key}")
+    public ResponseEntity<Set<String>> getFromSet(@PathVariable("key") String key){
+        Set<String> resultSet = mainService.getFromSet(key);
+        return ResponseEntity.ok().body(resultSet);
+    }
+
+    @PostMapping("/redis/hash")
+    public void addToHash(String key, String hashKey, String value){
+        mainService.addToHash(key, hashKey, value);
+    }
+
+    @GetMapping("/redis/hash/{key}/{hashKey}")
+    public ResponseEntity<String> getFromHash(@PathVariable("key") String key, @PathVariable("hashKey") String hashKey){
+        String resultHash = mainService.getFromHash(key, hashKey);
+        return ResponseEntity
+                .ok()
+                .body(resultHash);
+    }
+}
